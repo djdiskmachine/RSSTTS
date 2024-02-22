b217a115a4bcd4e179b207514c6aec431c0e3fde
djdiskmachine <djdiskmachine@proton.me>
2024-02-22 12:39:09 +0100

    Fix pipe to piper

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, subprocess, feedparser, time, re
 from pydub import AudioSegment
 
 TIMEOUT = 300
-PIPER_MODEL = " en_GB-aru-medium.onnx"
+PIPER_MODEL = "en_GB-alba-medium"
 RSS_FEED_URL = "https://pluralistic.net/feed/"
 REGEX_HYPERLINK = r'https?://(www.)?[-a-zA-Z0-9@:%.+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%+.~#?&//=]*)'
 
@@ -10,6 +10,14 @@ def fetch_feed(url):
     # result = feedparser.parse(url)
     result = feedparser.parse("./iTzxLVnH.rss")
     return result
+
+# Remove hyperlinks, html tags and self-plugs
+def clean_up_summary(summary):
+    summary = re.sub(r'.*?\(permalink\)', '', summary, count=1, flags=re.DOTALL)
+    summary = re.sub(REGEX_HYPERLINK, '', summary)
+    summary = re.sub(r'&#8211; ', '- ', summary)
+    summary = re.sub(r'This day in history.*', '', summary, flags=re.DOTALL)
+    return summary
 
 # Function to process feed entries
 def process_entries(entries):
@@ -23,18 +31,20 @@ def process_entries(entries):
             print(f"File '{name_txt}' already processed, continuing")
             continue
 
-        summary = entry.summary
-        summary = re.sub(r'.*?\(permalink\)', '', summary, count=1, flags=re.DOTALL)
-        summary = re.sub(REGEX_HYPERLINK, '', summary)
-        summary = re.sub(r'&#8211; ', '- ', summary)
-        summary = re.sub(r'This day in history.*', '', summary, flags=re.DOTALL)
+        summary = clean_up_summary(entry.summary)
         with open(name_txt, "w") as file:
             file.write(summary)
 
-        subprocess.run(["echo", name_txt, "|", "piper", "--model", PIPER_MODEL, "--output_file", name_wav])
+        with open(name_txt, 'r') as file_txt:
+            p1 = subprocess.Popen(["cat"], stdin=file_txt, stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(["piper", "-m", PIPER_MODEL, "--output_file", name_wav], stdin=p1.stdout, stdout=subprocess.PIPE)
+            p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+            p2.communicate()[0]
+
         input_wav = AudioSegment.from_file(name_wav)
         input_wav.export(name_mp3, format="mp3", bitrate="128k")
         os.remove(name_wav)
+        # os.remove(name_txt)
 
 # Main function
 def main():
